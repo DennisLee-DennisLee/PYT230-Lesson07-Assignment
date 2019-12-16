b70901742fc95b40ce6ede81196a6a5848a0875e
Dennis Lee <v-dele@microsoft.com>
2019-12-16 00:31:02 -0800

    Registering PostAdmin and CategoryAdmin.

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
-from blogging.models import Post, Category
+from blogging.models import Post, Category , PostAdmin , CategoryAdmin , CategoryInline
 
 
-admin.site.register(Post)
-admin.site.register(Category)
+# admin.site.register(Post)
+# admin.site.register(Category)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category, CategoryAdmin)
+# admin.site.register(PostAdmin)
+# admin.site.register(CategoryAdmin)
+# admin.site.register(CategoryInline)
